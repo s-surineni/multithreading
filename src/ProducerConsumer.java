2991a685de949e6a3ecfb2ba7293a56c634b1400
sampath surineni <sampath.surineni@gmail.com>
2021-12-03 20:48:43 +0530

    first commit

---
+++ src/ProducerConsumer.java
@@ -0,0 +1,17 @@
+//class Producter {
+//    public void produce() {
+//        while (isFull(buffer)) {
+//        }
+//        buffer[count++] = 1;
+//    }
+//}
+//
+//class Consumer {
+//    public void consume() {
+//        while (isEmpty(buffer)) {
+//        }
+//        buffer[--count] = 0;
+//    }
+//}
+//public class ProducerConsumer {
+//}
---
+++ src/RunCircularDep.java
@@ -0,0 +1,16 @@
+public class RunCircularDep {
+    public static void main(String[] args) throws InterruptedException {
+        CircularDepA cDep = new CircularDepA();
+        Runnable r1 = () -> cDep.a();
+        Runnable r2 = () -> cDep.b();
+
+        Thread t1 = new Thread(r1);
+        t1.start();
+
+        Thread t2 = new Thread(r2);
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
+}
